79c94972226c2c91f6f151de0ec42363f0359aff
Sofia <sofia.morgado@tecnico.ulisboa.pt>
2021-02-18 14:12:55 +0000

    added navbar and created page components

---
+++ src/pages/Starred.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// eslint-disable-next-line arrow-body-style
+const Starred = () => {
+    return (
+        <div>
+            This is Starred
+        </div>
+    )
+}
+
+export default Starred
