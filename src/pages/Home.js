58a34b2dd795ac691e2e988298cdce90ed6d2305
Sofia <sofia.morgado@tecnico.ulisboa.pt>
2021-02-18 21:30:48 +0000

    adding api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,41 @@
-import React from 'react'
-import MainPageLayout from '../components/MainPageLayout'
+import React, { useState } from 'react';
+import MainPageLayout from '../components/MainPageLayout';
 
 // eslint-disable-next-line arrow-body-style
 const Home = () => {
-    return (
-        <MainPageLayout>
-            This is Home
-        </MainPageLayout>
-    )
-}
+  const [input, setInput] = useState('');
 
-export default Home
+  // Change value as the user is writing
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onSearch = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(result => {});
+  };
+
+  const onKeyDown = ev => {
+    // if user pressed Enter
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
+};
+
+export default Home;
